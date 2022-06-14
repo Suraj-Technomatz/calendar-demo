bfd8d8bfbeedd9db24e610cbb93b7727540b46da
Suraj Verma <sverma@technomatz.com>
2022-06-14 19:10:59 +0530

    Add layouts

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 // Getting data from files
 import ApiData from "./data";
+import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      data: ApiData
-    }
+      data: ApiData,
+    };
   }
 
   render() {
     return (
-      <div>
-        Hello
-      </div>
-    )
+      <Container fluid>
+        <div className="main_container">
+          <Row>
+            <Col md={4}>
+              <div className="programs_views">PRograms List</div>
+            </Col>
+            <Col md={8}>
+              <div className="calendar_views">Calendar List</div>
+            </Col>
+          </Row>
+        </div>
+      </Container>
+    );
   }
 }
 
